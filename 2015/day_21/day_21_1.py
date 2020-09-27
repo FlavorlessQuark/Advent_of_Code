5da7a7cd96500c9d8a82697be24180e52d6e1be1
FlavorlessQuark <mahthaki@gmail.com>
2020-09-27 00:14:22 -0700

    The only reason I got this start is because of a typo. Still I refuse to do a fight simulation when I can use math

--- 2015/day_21/day_21_1.py
+++ 2015/day_21/day_21_1.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 Weapons ={(8,4, 0),
 		(10, 5, 0),
@@ -23,7 +24,7 @@ Rings ={
 (40, 0, 2),
 (80, 0, 3)}
 
-minimum = 800
+minimum = -1
 with open('input') as input:
 	boss = [int(line.split()[-1]) for line in input]
 
@@ -33,22 +34,14 @@ for weapon in Weapons:
 		a_total = boss[1] -  armour[2]
 		a_total = a_total if a_total > 0 else 1
 		cost = weapon[0] + armour[0]
-		# print(boss[0] / d_total, 100 / a_total)
-		if (boss[0] / d_total) <= (100 / a_total):
-			minimum = min(minimum, cost)
+		if (boss[0] / d_total) >= (100 / a_total):
+			minimum = max(minimum, cost)
 		for r1,r2 in itertools.combinations(Rings,2):
-			# print(r1, r2)
-			d_total = weapon[1] + r1[1] + r2[1] - boss[2]
-			a_total = boss[1] - (r1[2] + r2[2] + armour[2])
-			a_total = a_total if a_total > 0 else 1
+			hero_damage = r1[1] + r2[1] + weapon[1] - boss[2]
+			boss_damage = max((boss[1] - (r1[2] + r2[2] + armour[2])), 1)
 			cost = weapon[0] + armour[0] + r1[0] + r2[0]
-			# print(boss[0] / d_total, 100 / a_total)
-			if (boss[0] / d_total) <= (100 / a_total):
-				minimum = min(minimum, cost)
-				# print(d_total)
-				# print(a_total)
-		# exit()
-
+			if (boss[0] / hero_damage) >= (100 / boss_damage):
+				minimum = max(minimum, cost)
 print(minimum)
 
 
