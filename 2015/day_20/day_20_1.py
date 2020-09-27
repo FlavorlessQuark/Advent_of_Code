365df6748c20dfdcfe39e53df479f880c6f91455
FlavorlessQuark <mahthaki@gmail.com>
2020-09-26 18:04:19 -0700

    Day20 p1. Runs in 35 sec. Huge improvement from before. Come back with prime number magic for faster stuff

--- 2015/day_20/day_20_1.py
+++ 2015/day_20/day_20_1.py
@@ -1,14 +1,17 @@
-from __future__ import division
+
 import numpy as np
 
 goal = 34000000
 
-houses = np.zeros(goal + 1)
+houses = np.zeros(goal)
 
-for num in xrange(1, goal + 1):
-	for i in xrange(0, goal + 1, num):
-		houses[i] += num
+for num in xrange(1, goal / 10):
+	for i in xrange(0, goal / 10, num):
+		houses[i] += num * 10
 		if houses[i] >= goal and i > 0:
-			print(i)
-			exit()
+			break
 
+for x in range(0, goal):
+	if houses[x] >= goal and x > 0:
+		print(x)
+		break
