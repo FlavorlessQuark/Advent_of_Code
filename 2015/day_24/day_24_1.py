1f7180e561b006d9649cec921746de3370601d45
FlavorlessQuark <mahthaki@gmail.com>
2020-09-28 00:59:34 -0700

    Day 24 easier than day1

--- 2015/day_24/day_24_1.py
+++ 2015/day_24/day_24_1.py
@@ -1,14 +1,21 @@
 import itertools
 
-config = [50,80000]
+config = [5000,3018894606110]
 
 with open('input') as input:
 	pkg = [int(line[:-1]) for line in input]
 
-weight = sum(pkg) / 3
+weight = sum(pkg) / 4
 
-for i in range len(pkg):
-	for comb in itertools.combinations():
-		
+for i in range (2, len(pkg) - 1):
+	for comb in itertools.combinations(pkg, i):
+		if sum(comb) != weight:
+			continue
+		if config[0] >= i:
+			config[0] = i
+			config[1] = min(config[1], reduce(lambda x, y: x*y, comb))
+			# print(comb)
+		else:
+			break
 
-print(weight)
+print(config)
