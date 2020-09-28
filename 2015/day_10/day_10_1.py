9cd3cd37e17b9774b7f58bbb35c197796ae23065
FlavorlessQuark <mahthaki@gmail.com>
2020-09-28 04:24:50 -0700

    Something to be done with regex here. finish tomorrow

--- 2015/day_10/day_10_1.py
+++ 2015/day_10/day_10_1.py
@@ -1,27 +1,13 @@
-num_str =""
+import re
+
+# def count(str)
+# {
+# }
 
 with open("input") as input:
-	num_str = input.readline()
-for i in range(0,50):
-	save = num_str[0]
-	count = 0
-	num = 0
-	for n in range(0, len(num_str)):
-		if save == num_str[n]:
-			count += 1
-		else:
-			num *= 10
-			num += count
-			num *= 10
-			num += int(save)
-			save = num_str[n]
-			count = 1
-	if str.isdigit(num_str[n]):
-		num *= 10
-		num += count
-		num *= 10
-		num += int(save)
-		save = num_str[n]
-		count = 0
-	num_str = str(num)
-print(str(len(num_str)))
+	num_str = [int(char) for line in input for char in line[:-1]]
+
+exp = re.compile(r'\d')
+# for i in range(0,3):
+	# re.sub()
+# print(num_str)
