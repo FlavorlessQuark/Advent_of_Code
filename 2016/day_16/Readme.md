a34b3413bc59e5fe6da6158a38edcc84cda52520
OSXVM <osxvm@OSXVMs-Mac.local>
2020-10-09 21:07:36 -0700

    Day 16

---
+++ 2016/day_16/Readme.md
@@ -0,0 +1,6 @@
+I was thinking it would be possible to create some new type with 272 bits...
+I'm not sure how useful it would be, but it would be interesting to try regardless
+
+p1 gives a result almost instantly while part 2 doesnt give anything even after an hour.
+Not sure if code or VM.
+There is a memory allocation that might be the problem in strrev. Something something macro magic? I had seen a few SWAP macros on stackoverflow, I might try to work out something similar. Maybe without the extra memory allocation, it will actually run faster
---
+++ 2016/day_16/p1.c
@@ -0,0 +1,91 @@
+#include "../aoc.h"
+
+//This whole thing looks like a bit problem, however no type can holde that many bits
+//Unsure about how to create said type, but it can probably be done
+//To do later in AoC+
+
+char *strrev(char *str)
+{
+	int i, len;
+	char *cpy;
+
+	cpy = join("",str); //Not a fan of the extra allocation here
+	i = 0;
+	len = strlen(str) - 1;
+	while (len >= 0)
+	{
+		str[i] = cpy[len];
+		i++;
+		len--;
+	}
+	return str;
+}
+
+void replace(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == '1')
+			str[i] = '0';
+		else
+			str[i] = '1';
+		i++;
+	}
+}
+
+void checksum(char *str)
+{
+	int i;
+	int n;
+	while (strlen(str) %2 == 0)
+	{
+		n = 0;
+		i = 1;
+	//	printf("str :%s\n", str);
+		while (i < strlen(str))
+		{
+			if (str[i] == str[i - 1])
+				str[n] = '1';
+			else
+				str[n] = '0';
+			n++;
+			i += 2;
+		}
+		str[n] = '\0';
+	}
+}
+
+char *fill()
+{
+	char *a;
+	char *b;
+	int end;
+
+	end = 272;
+	a = join("", "10001001100000001");
+
+	while (strlen(a) < end)
+	{
+		b = strrev(strdup(a));
+		replace(b);
+		a = join(join(a, "0"), b);
+	}
+	a[end] = '\0';
+//	printf("a :%s\n", a);
+	return a;
+}
+
+int main()
+{
+	char *result;
+
+	result = fill();
+	checksum(result);
+
+	printf("Result %s\n", result);
+}
+
