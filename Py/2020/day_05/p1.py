9ee08d1437825bcc7d739aba1d1e7435bdb4f828
FlavorlessQuark <mahthaki@gmail.com>
2020-12-04 22:07:51 -0800

    Day 05 refactored

--- Py/2020/day_05/p1.py
+++ Py/2020/day_05/p1.py
@@ -3,17 +3,6 @@ with open('input') as input:
 
 maximum = 0
 for x in inp:
-	row = [n for n in range(128)]
-	col = [n for n in range(8)]
-	for letter in x:
-		if letter == 'F':
-			row = row[:len(row) // 2]
-		elif letter == 'B':
-			row = row[len(row) // 2:]
-		if letter == 'R':
-			col = col[len(col) // 2:]
-		elif letter == 'L':
-			col = col[:len(col) // 2]
-		maximum = max(row[0] * 8 + col[0], maximum)
+	ids = "".join(['1' if letter == 'B' or letter == 'R' else '0' for letter in x])
+	maximum = max(int(ids, 2), maximum)
 print("Solution : ", maximum)
-
