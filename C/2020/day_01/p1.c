fef9c2256dbcd961a743cdf30905f19dd36446c7
FlavorlessQuark <mahthaki@gmail.com>
2020-12-04 23:27:14 -0800

    Day 01

---
+++ C/2020/day_01/p1.c
@@ -0,0 +1,16 @@
+#include "../../aoc.h"
+
+
+int	main()
+{
+	int		i;
+	int		*num;
+	char	**input;
+
+	i = fetch_input("input", &input);
+
+	MAP(input, atoi, i, num);
+
+	for(int n = 0; n < i; n++)
+		FOREACH(ITER < i, if (num[ITER] +num[n] == 2020){printf("%d\n", num[ITER] * num[n]);return 1;})
+}
---
+++ C/2020/day_01/p2.c
@@ -0,0 +1,17 @@
+#include "../../aoc.h"
+
+int	main()
+{
+	int		i;
+	int		*num;
+	char	**input;
+
+	i = fetch_input("input", &input);
+
+	MAP(input, atoi, i, num);
+
+	for(int x = 0; x < i; x++)
+		for(int n = 0; n < i; n++)
+			FOREACH(ITER < i, if (num[ITER] + num[n] + num[x] == 2020)
+				{printf("%d\n",   num[ITER] * num[n] * num[x]); return 1;})
+}
