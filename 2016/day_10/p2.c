c5d6a1af7097be2e4708ed7ff5df0fb0a5226999
flavorlessquark <flavorlessquark@gmail.com>
2022-08-31 13:02:37 -0700

    Day10 p2

--- 2016/day_10/p2.c
+++ 2016/day_10/p2.c
@@ -1,6 +1,7 @@
 #include "../../aoc.h"
 
-
+# define OFFSET_TO_LOW_TARGET 14
+# define OFFSET_TO_HIGH_TARGET 13
 typedef struct s_bot
 {
 	int high;
@@ -8,6 +9,10 @@ typedef struct s_bot
 	int given;
 	char *instruction;
 }				bot;
+
+
+void execGiveInstruction(int botId, bot *bots, int * outputs);
+
 
 int getMaxNumber(_Node *list)
 {
@@ -45,6 +50,18 @@ void giveValToBot(int val, bot *bot)
 	}
 }
 
+void printBotsAndOutputs(bot *bots, int *outputs, int count)
+{
+	int i;
+
+	i = 0;
+	while (i < count)
+	{
+		printf("Bot %d (%d, %d) | output %d : %d\n", i, bots[i].low, bots[i].high, i, outputs[i]);
+		i++;
+	}
+}
+
 _Node *formatBotsAndInstructions(_Node *instructions, bot *bots)
 {
 	_Node *head;
@@ -54,7 +71,6 @@ _Node *formatBotsAndInstructions(_Node *instructions, bot *bots)
 	head = NULL;
 	while (instructions)
 	{
-		printf("??? %p %p\n",instructions, instructions->next);
 		if (((char *)instructions->data)[0] =='v')
 		{
 			if (!head)
@@ -70,7 +86,7 @@ _Node *formatBotsAndInstructions(_Node *instructions, bot *bots)
 		}
 		else
 		{
-			extract_num(instructions->data, &botID);
+			instructions->data +=  extract_num(instructions->data, &botID);
 			bots[botID].instruction = instructions->data;
 			bots[botID].low = -1;
 			bots[botID].high = -1;
@@ -82,6 +98,56 @@ _Node *formatBotsAndInstructions(_Node *instructions, bot *bots)
 	return head;
 }
 
+void checkBotAndExec(int botId, bot *bots, int * outputs)
+{
+	if (bots[botId].low > -1 && bots[botId].high > -1)
+		execGiveInstruction(botId, bots, outputs);
+}
+
+void execValueInstruction(char *instruction, bot *bots, int *outputs)
+{
+	int value;
+	int botId;
+
+	instruction += extract_num(instruction, &value);
+	extract_num(instruction, &botId);
+	giveValToBot(value, &bots[botId]);
+	checkBotAndExec(botId, bots, outputs);
+}
+
+void execGiveInstruction(int botId, bot *bots, int * outputs)
+{
+	int id;
+	int offset;
+	char *instruction;
+
+	instruction = bots[botId].instruction;
+	offset = extract_num(instruction, &id);
+
+	printf("\nExec bot %d : %s\n", botId, instruction);
+	if (instruction[OFFSET_TO_LOW_TARGET] == 'o')
+		outputs[id] = bots[botId].low;
+	else
+	{
+		giveValToBot(bots[botId].low, &bots[id]);
+		checkBotAndExec(id, bots, outputs);
+	}
+	bots[botId].low = -1;
+
+
+	instruction += offset;
+	extract_num(instruction, &id);
+
+	if (instruction[OFFSET_TO_HIGH_TARGET] == 'o')
+		outputs[id] = bots[botId].high;
+	else
+	{
+		giveValToBot(bots[botId].high, &bots[id]);
+		checkBotAndExec(id, bots, outputs);
+	}
+	bots[botId].high = -1;
+}
+
 int main()
 {
 	_Node *instructions;
@@ -91,9 +157,9 @@ int main()
 	size_t len;
 	int maxNum;
 
-	instructions = fetch_by_word("test", "\n", 1, &len);
+	instructions = fetch_by_word("input", "\n", 1, &len);
 
-	maxNum = getMaxNumber(instructions);
+	maxNum = getMaxNumber(instructions) + 1;
 	bots = malloc(maxNum * sizeof(bot));
 	outputs = calloc(maxNum,  sizeof(int));
 
@@ -102,12 +168,8 @@ int main()
 	while(valueInstructions)
 	{
 		printf("%s\n", valueInstructions->data);
+		execValueInstruction(valueInstructions->data, bots, outputs);
 		valueInstructions = valueInstructions->next;
 	}
-	int i = 0;
-	while(i < maxNum)
-	{
-		printf("Bot %d : %s  | output %d %d\n", i, bots[i].instruction, i , outputs[i]);
-		i++;
-	}
+	printf("Value %d\n", outputs[0] * outputs[1] * outputs[2]);
 }
